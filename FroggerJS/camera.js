305d96af8c8c0eb83438e5c012bbfceb021d32e0
Filipe Relvas <frelvas5@gmail.com>
2014-12-14 23:05:28 +0000

    House keeping

--- FroggerJS/camera.js
+++ FroggerJS/camera.js
@@ -94,8 +94,6 @@ Camera.prototype.draw = function(camX, camY, camZ, oX, oY, oZ, side) {
 			mat4.identity(vMatrix);
 			mat4.identity(mMatrix);
 
-			console.log((1 / Math.tan(60.0/2))/30);
-		
 			mat4.lookAt([oX - 10.0, oY + 6.0, oZ + 0.005], [oX - camX, oY - camY, oZ - camZ], [0.0, 1.0, 0.0], vMatrix);
 		}
 	}
